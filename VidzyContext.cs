6f9c3ce7aacaf53d4d8d88db87c7d377a0ecea42
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-05-06 18:51:29 -0400

    created initial model class

---
+++ VidzyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vidzy.Models;
+
+namespace Vidzy
+{
+    class VidzyContext : DbContext
+    {
+        public DbSet<Video> Videos {get; set;}
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
